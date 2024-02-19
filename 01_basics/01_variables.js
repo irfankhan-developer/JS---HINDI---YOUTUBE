5cdb329088304cdf1ffa0d76b29cee7814a5da3f
irfankhan-developer <158798845+irfankhan-developer@users.noreply.github.com>
2024-02-19 18:34:00 +0000

    commit changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 101
+let accountEmail = "patanirfankhan74@gmail.com"
+var accountcity  = "Bengaluru"
+accountPassword = 2623
+// accountId = 102 
+console.log(accountId);
+/*
+we will not use var keyword because issue in block scope or function scope
+*/
+accountEmail="patankhan@gmail.com"
+accountcity="chennai"
+accountPassword="834080"
+console.table([accountId,accountEmail,accountcity,accountPassword])
